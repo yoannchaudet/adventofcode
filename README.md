09f12daab6a85b379dbd110b077a1bcb7694ab0d
Yoann Chaudet <yoannchaudet@github.com>
2020-12-05 15:32:06 -0800

    Day 01

---
+++ README.md
@@ -0,0 +1,14 @@
+# adventofcode-2020
+
+I like puzzle and I am probably very rusty 🧠.
+
+Submissions for https://adventofcode.com.
+
+## Day-01
+
+For that day, I am using PowerShell 7.1:
+
+``` ps
+./day-01/fix-accounting.ps1 ./day-01/input.txt
+./day-01/fix-accounting.ps1 ./day-01/input.txt -Part2
+```
---
+++ day-01/fix-accounting.ps1
@@ -0,0 +1,85 @@
+#!/usr/bin/env pwsh
+#Requires -Version 7.1
+
+<#
+.SYNOPSIS
+Find and print the two entries adding up to 2020 in the expense report
+and multiply them.
+
+.PARAMETER ExpenseReport
+Path to the expense report.
+
+.PARAMETER Part2
+Part two: find and print three entries adding up to 2020.
+#>
+
+param (
+  [Parameter(Mandatory=$true)]
+  [string] $ExpenseReport,
+  [switch] $Part2
+)
+
+# Init
+Set-StrictMode -version 'Latest'
+$ErrorActionPreference = 'Stop'
+
+function Get-TwoEntries2020 {
+  param ([int[]]$Entries)
+
+  # a + b = 2020
+  # go through the list (fix a) and check if b is in it
+  foreach ($a in $entries) {
+    # Deduct b
+    $b = 2020 - $a
+
+    # If b is in the list, we are done
+    if ($Entries -contains $b) {
+      return [PSCustomObject]@{
+        A = $a
+        B = $b
+        Result = $a * $b
+      }
+    }
+  }
+}
+
+function Get-ThreeEntries2020 {
+  param ([int[]]$Entries)
+
+  # Assume there is at least 3 entries available
+  $min = $Entries[0]
+  $max = $Entries[-1]
+
+  # a + b + c = 2020
+  # go through the list (fix a), fix b (with b < 2020 - a), deduct c and check if in the list
+  foreach ($a in $entries) {
+    $bCandidates = $entries | Where-Object { $_ -lt 2020 - $a }
+    foreach ($b in $bCandidates) {
+      $c = 2020 - $a - $b
+      # If b is in the list, we are done
+      if ($Entries -contains $c) {
+        return [PSCustomObject]@{
+          A = $a
+          B = $b
+          C = $c
+          Result = $a * $b * $c
+        }
+      }
+    }
+  }
+}
+
+#
+# Main
+#
+
+# Read the expense report, convert lines to int and sort everything
+# Assume all entries are unique
+$entries = Get-Content $ExpenseReport | ForEach-Object { [int] $_ } | Sort-Object
+
+$result = $Part2 ? (Get-ThreeEntries2020 $entries) : (Get-TwoEntries2020 $entries)
+if ($result) {
+  $result | Format-Table
+} else {
+  Write-Host "No results found"
+}
