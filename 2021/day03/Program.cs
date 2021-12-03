56b1cbae8ccadd34c7ac9822fd1d22ea9d996b7c
Yoann Chaudet <yoannchaudet@github.com>
2021-12-02 22:15:18 -0800

    Day 3 part 2

--- 2021/day03/Program.cs
+++ 2021/day03/Program.cs
@@ -1,30 +1,69 @@
-﻿// Get the commands from the file (as string)
-static (int, List<string>) GetCommands(string inputPath) {
-  var lines = File.ReadAllLines(inputPath).Where(line => !string.IsNullOrWhiteSpace(line));
-  return (lines.First().Length, lines.ToList());
+﻿// Given a list of binary number of same length, return an array containing
+// a 1 at a given position to indicate 0 was the most common bit.
+static int[] GetMostCommonZeroBits(List<string> binaryNumbers) {
+  int[] zeroBits = new int[binaryNumbers.First().Length];
+  foreach (var binaryNumber in binaryNumbers) {
+    for (int i = 0; i < zeroBits.Length; i++) {
+      if (binaryNumber[i] == '0') {
+        zeroBits[i]++;
+      }
+    }
+  }
+  return zeroBits;
+}
+
+// Return the most common bit (1 or 0) for a given zero-bit count.
+static int GetMostCommonBit(int zeroBits, int commandsCount) {
+  return zeroBits <= commandsCount / 2 ? 1 : 0;
+}
+
+// Return the least common bit (1 or 0) for a given zero-bit count.
+static int GetLeastCommonBit(int zeroBits, int commandsCount) {
+  return GetMostCommonBit(zeroBits, commandsCount) == 0 ? 1 : 0;
+}
+
+// Given a most common zero bit array, return the most common base10 number.
+static int GetCommonBase10(int[] mostCommonZeroBits, int commandsCount, int commonBit) {
+  int numberBase10 = 0;
+  for (int i = 0; i < mostCommonZeroBits.Length; i++) {
+   // Most common bit for position i is 1
+   if (GetMostCommonBit(mostCommonZeroBits[i], commandsCount) == commonBit) {
+     numberBase10 += (int)Math.Pow(2, mostCommonZeroBits.Length - i - 1);
+   }
+  }
+  return numberBase10;
 }
 
 // Get the commands and compute the most common bits
-var commands = GetCommands("./inputs/input.txt");
-int[] zeroBits = new int[commands.Item1];
-foreach (var command in commands.Item2) {
-  for (int i = 0; i < zeroBits.Length; i++) {
-    if (command[i] == '0') {
-      zeroBits[i]++;
-    }
-  }
-}
-
-int gammaRateBase10 = 0;
-int epsilonRateBase10 = 0;
-for (int i = 0; i < zeroBits.Length; i++) {
-   // Most common bit for position i is 1
-   if (zeroBits[i] < commands.Item2.Count / 2) {
-    gammaRateBase10 += (int)Math.Pow(2, zeroBits.Length - i - 1);
-   } else {
-    epsilonRateBase10 += (int)Math.Pow(2, zeroBits.Length - i - 1);
-   }
-}
+var inputPath = "./inputs/input.txt";
+var commands = File.ReadAllLines(inputPath).ToList();
+int[] mostCommonZeroBits = GetMostCommonZeroBits(commands);
+int gammaRateBase10 = GetCommonBase10(mostCommonZeroBits, commands.Count, 1);
+int epsilonRateBase10 = GetCommonBase10(mostCommonZeroBits, commands.Count, 0);
 Console.WriteLine("Gamma rate = {0}", gammaRateBase10);
 Console.WriteLine("Epsilon rate = {0}", epsilonRateBase10);
-Console.WriteLine("Result = {0}", gammaRateBase10 * epsilonRateBase10);
+Console.WriteLine("Result (part1) = {0}", gammaRateBase10 * epsilonRateBase10);
+
+int index = 0;
+var ogrPrefix = GetMostCommonBit(mostCommonZeroBits[index++], commands.Count).ToString();
+var ogrCandidates = commands.Where(c => c.StartsWith(ogrPrefix)).ToList();
+while (ogrCandidates.Count > 1) {
+  int[] candidatesMostCommonZeroBits = GetMostCommonZeroBits(ogrCandidates);
+  ogrPrefix += GetMostCommonBit(candidatesMostCommonZeroBits[index++], ogrCandidates.Count).ToString();
+  ogrCandidates = ogrCandidates.Where(c => c.StartsWith(ogrPrefix)).ToList();
+}
+var ogr = Convert.ToInt32(ogrCandidates.First(), 2);
+Console.WriteLine("OGR = {0}", ogr);
+
+index = 0;
+var csrPrefix = GetLeastCommonBit(mostCommonZeroBits[index++], commands.Count).ToString();
+var csrCandidates = commands.Where(c => c.StartsWith(csrPrefix)).ToList();
+while (csrCandidates.Count > 1) {
+  int[] candidatesMostCommonZeroBits = GetMostCommonZeroBits(csrCandidates);
+  csrPrefix += GetLeastCommonBit(candidatesMostCommonZeroBits[index++], csrCandidates.Count).ToString();
+  csrCandidates = csrCandidates.Where(c => c.StartsWith(csrPrefix)).ToList();
+}
+var csr = Convert.ToInt32(csrCandidates.First(), 2);
+Console.WriteLine("CSR = {0}", csr);
+
+Console.WriteLine("Life support rating (part 2) = {0}", ogr * csr);
