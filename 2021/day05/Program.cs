601c01a4d18bbca1f84c180cadb1b21751e71916
Yoann Chaudet <yoannchaudet@github.com>
2021-12-05 08:54:09 -0800

    Day 5 part 2

--- 2021/day05/Program.cs
+++ 2021/day05/Program.cs
@@ -1,40 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var inputPath = "./inputs/input.txt";
+ComputeSegments(inputPath, false);
+ComputeSegments(inputPath, true);
 
-var inputPath = "./inputs/input.txt";
-var coordinates = File.ReadAllLines(inputPath);
-var segments = coordinates.Select(c => new Segment(c)).ToList();
-
-// Create diagram
-int maxX = segments.SelectMany(s => s.PointsInSegment).Max(p => p.X);
-int maxY = segments.SelectMany(s => s.PointsInSegment).Max(p => p.Y);
-int[][] diagram = new int[maxX + 1][];
-for (int x = 0; x <= maxX; x++)
+static void ComputeSegments(string inputPath, bool computeDiagonals)
 {
-  diagram[x] = new int[maxY + 1];
+  var coordinates = File.ReadAllLines(inputPath);
+  var segments = coordinates.Select(c => new Segment(c, computeDiagonals)).ToList();
+
+  // Create diagram
+  int maxX = segments.SelectMany(s => s.PointsInSegment).Max(p => p.X);
+  int maxY = segments.SelectMany(s => s.PointsInSegment).Max(p => p.Y);
+  int[][] diagram = new int[maxY + 1][];
   for (int y = 0; y <= maxY; y++)
   {
-    diagram[x][y] = 0;
+    diagram[y] = new int[maxX + 1];
+    for (int x = 0; x <= maxX; x++)
+    {
+      diagram[y][x] = 0;
+    }
   }
+  segments.SelectMany(s => s.PointsInSegment).ToList().ForEach(p => diagram[p.Y][p.X]++);
+  // Don't print diagram (too big in part 2)
+  // PrintDiagram(diagram);
+  var overlapCounts = diagram.SelectMany(row => row).Where(c => c >= 2).Count();
+  Console.WriteLine("Result (compute diagonals = {1}) = {0}", overlapCounts, computeDiagonals);
 }
-segments.SelectMany(s => s.PointsInSegment).ToList().ForEach(p => diagram[p.X][p.Y]++);
-// PrintDiagram(diagram);
-var overlapCounts = diagram.SelectMany(row => row).Where(c => c >= 2).Count();
-Console.WriteLine("Result (part1) = {0}", overlapCounts);
 
 static void PrintDiagram(int[][] diagram)
 {
-  for (int x = 0; x < diagram.Length; x++)
+  for (int y = 0; y < diagram.Length; y++)
   {
-    for (int y = 0; y < diagram[x].Length; y++)
+    for (int x = 0; x < diagram[y].Length; x++)
     {
-      if (diagram[x][y] == 0)
+      if (diagram[y][x] == 0)
       {
         Console.Write(".");
       }
       else
       {
-        Console.Write(diagram[x][y]);
+        Console.Write(diagram[y][x]);
       }
     }
     Console.WriteLine();
@@ -56,13 +60,17 @@ class Point
 
 class Segment
 {
+  private bool computeDiagonals;
+
   public Point Start { get; private set; }
   public Point End { get; private set; }
 
   public List<Point> PointsInSegment { get; private set; }
 
-  public Segment(string coordinates)
+  public Segment(string coordinates, bool computeDiagonals)
   {
+    this.computeDiagonals = computeDiagonals;
+
     var parts = coordinates.Split("->");
     Start = new Point(parts[0]);
     End = new Point(parts[1]);
@@ -89,6 +97,20 @@ class Segment
       {
         pointsInSegment.Add(new Point($"{x},{Start.Y}"));
       }
+    }
+
+    // Diagonal
+    else if (computeDiagonals)
+    {
+      var dx = Start.X > End.X ? -1 : 1;
+      var dy = Start.Y > End.Y ? -1 : 1;
+      int x = Start.X, y = Start.Y;
+      do
+      {
+        pointsInSegment.Add(new Point($"{x},{y}"));
+        x += dx;
+        y += dy;
+      } while (x != End.X + dx && y != End.Y + dy);
     }
 
     return pointsInSegment;
