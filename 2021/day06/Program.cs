8951a0fb258e64c9fb77fad10103b9c6aad6b388
Yoann Chaudet <yoannchaudet@github.com>
2021-12-06 15:56:14 -0800

    Day 6 part 2 (brute force)

--- 2021/day06/Program.cs
+++ 2021/day06/Program.cs
@@ -1,26 +1,32 @@
 ﻿
+using System.Numerics;
+
 var inputPath = "./inputs/input.txt";
-var afterDays = 80;
+var afterDays = 256;
 var timers = File.ReadAllLines(inputPath).Where(line => line.Length > 0).First().Split(',').Select(int.Parse).ToList();
-var population = timers.Select(timer => GetLanternFishes(timer, afterDays)).Sum();
+var groupedTimers = timers.GroupBy(x => x);
+var population = groupedTimers.Select(timerGroup => timerGroup.Count() * GetLanternFishes(timerGroup.Key, afterDays)).Sum();
 Console.WriteLine("Lantern fishes (at day {0}) = {1}", afterDays, population);
 
 // Get the number of lantern fishes after a number of days (for a given initial timer).
-static int GetLanternFishes(int timer, int afterDays)
+static long GetLanternFishes(int timer, int afterDays)
 {
   // Self
-  var population = 1;
+  var population = (long) 1;
+
+  int delta = 1;
 
   while (afterDays > 1)
   {
-    timer--;
-    afterDays--;
+    timer -= delta;
+    afterDays -= delta;
 
-    if (timer == 0)
+    if (timer == 0 && afterDays >= 1)
     {
       // New fish + reset timer
       population += GetLanternFishes(8 + 1, afterDays);
       timer = 6 + 1;
+      delta = timer;
     }
   }
 
