28f108d169197cf43682972f3ab47130dbc55a38
Yoann Chaudet <yoannchaudet@github.com>
2021-12-06 13:17:45 -0800

    Day 6 part 1

---
+++ 2021/day06/Program.cs
@@ -0,0 +1,28 @@
+﻿
+var inputPath = "./inputs/input.txt";
+var afterDays = 80;
+var timers = File.ReadAllLines(inputPath).Where(line => line.Length > 0).First().Split(',').Select(int.Parse).ToList();
+var population = timers.Select(timer => GetLanternFishes(timer, afterDays)).Sum();
+Console.WriteLine("Lantern fishes (at day {0}) = {1}", afterDays, population);
+
+// Get the number of lantern fishes after a number of days (for a given initial timer).
+static int GetLanternFishes(int timer, int afterDays)
+{
+  // Self
+  var population = 1;
+
+  while (afterDays > 1)
+  {
+    timer--;
+    afterDays--;
+
+    if (timer == 0)
+    {
+      // New fish + reset timer
+      population += GetLanternFishes(8 + 1, afterDays);
+      timer = 6 + 1;
+    }
+  }
+
+  return population;
+}
