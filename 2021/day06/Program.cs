15086bd0ff5a2cbecb2a0f53ae8556dd360bfba2
Yoann Chaudet <yoannchaudet@github.com>
2021-12-06 18:36:13 -0800

    Optimnize the solution with a cache:

--- 2021/day06/Program.cs
+++ 2021/day06/Program.cs
@@ -5,17 +5,17 @@ var inputPath = "./inputs/input.txt";
 var afterDays = 256;
 var timers = File.ReadAllLines(inputPath).Where(line => line.Length > 0).First().Split(',').Select(int.Parse).ToList();
 var groupedTimers = timers.GroupBy(x => x);
-var population = groupedTimers.Select(timerGroup => timerGroup.Count() * GetLanternFishes(timerGroup.Key, afterDays)).Sum();
+var cache = new Dictionary<(int, int), long>();
+var population = groupedTimers.Select(timerGroup => timerGroup.Count() * GetLanternFishes(timerGroup.Key, afterDays, cache)).Sum();
 Console.WriteLine("Lantern fishes (at day {0}) = {1}", afterDays, population);
 
 // Get the number of lantern fishes after a number of days (for a given initial timer).
-static long GetLanternFishes(int timer, int afterDays)
+static long GetLanternFishes(int timer, int afterDays, IDictionary<(int, int), long> cache)
 {
   // Self
   var population = (long) 1;
 
   int delta = 1;
-
   while (afterDays > 1)
   {
     timer -= delta;
@@ -23,12 +23,16 @@ static long GetLanternFishes(int timer, int afterDays)
 
     if (timer == 0 && afterDays >= 1)
     {
-      // New fish + reset timer
-      population += GetLanternFishes(8 + 1, afterDays);
+      // New fish
+      var key = (8 + 1, afterDays);
+      if (!cache.ContainsKey(key))
+        cache.Add(key, GetLanternFishes(8 + 1, afterDays, cache));
+      population += cache[key];
+
+      // Reset timer (+ optimize delta)
       timer = 6 + 1;
       delta = timer;
     }
   }
-
   return population;
 }
