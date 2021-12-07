c33aa2a592193c53704caca42886ad79895be5fa
Yoann Chaudet <yoannchaudet@github.com>
2021-12-06 21:47:26 -0800

    Day 7 part 2 (ugly)

--- 2021/day07/Program.cs
+++ 2021/day07/Program.cs
@@ -5,6 +5,9 @@ var min = positions.Min();
 var max = positions.Max();
 var avg = (int)positions.Average();
 
+// Part 1
+// Start at the average and find local minimum in both directions (go through half the dataset).
+
 var hlTop = new List<int>();
 for (var horizontalLocation = avg; horizontalLocation <= (max / 2); horizontalLocation++)
 {
@@ -18,9 +21,40 @@ for (var horizontalLocation = avg; horizontalLocation >= (min / 2); horizontalLo
 }
 
 var minimumFuel = Math.Min(hlTop.Min(), hlBottom.Min());
-Console.WriteLine("Minimum fuel: {0}", minimumFuel);
+Console.WriteLine("Minimum fuel (part 1): {0}", minimumFuel);
 
+hlTop = new List<int>();
+for (var horizontalLocation = avg; horizontalLocation <= (max / 2); horizontalLocation++)
+{
+  hlTop.Add(GetCost2(positions, horizontalLocation));
+}
+
+hlBottom = new List<int>();
+for (var horizontalLocation = avg; horizontalLocation >= (min / 2); horizontalLocation--)
+{
+  hlBottom.Add(GetCost2(positions, horizontalLocation));
+}
+
+minimumFuel = Math.Min(hlTop.Min(), hlBottom.Min());
+Console.WriteLine("Minimum fuel (part 2): {0}", minimumFuel);
+
+// Simple cost function (part 1)
 static int GetCost(int[] positions, int horizontalLocation)
 {
   return positions.Select(x => Math.Abs(x - horizontalLocation)).Sum();
+}
+
+// Complex cost function (part 2)
+static int GetCost2(int[] positions, int horizontalLocation)
+{
+  return positions.Select(x => GetDistanceCost(Math.Abs(x - horizontalLocation))).Sum();
+}
+
+// Cost for a given traveled distance (part 2)
+static int GetDistanceCost(int distance) {
+  var cost = 0;
+  for (int i = 1; i <= distance; i++) {
+    cost += i;
+  }
+  return cost;
 }
