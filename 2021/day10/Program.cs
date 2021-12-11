dcecc094dd6e6d21d411bb874eb278edc641a9da
Yoann Chaudet <yoannchaudet@github.com>
2021-12-10 22:50:56 -0800

    Day 10 part 1

---
+++ 2021/day10/Program.cs
@@ -0,0 +1,56 @@
+﻿var inputPath = "./inputs/input.txt";
+var syntaxScores = new Dictionary<char, int>() {
+  { ')', 3 },
+  { ']', 57 },
+  { '}', 1197 },
+  { '>', 25137 },
+};
+var totalSyntaxErrorScore = File.ReadAllLines(inputPath)
+  .Select(line => GetFirstIllegalCharacter(line))
+  .Where(c => c.HasValue)
+  .Select(c => syntaxScores[c.Value])
+  .Sum();
+Console.WriteLine("Total syntax error score: {0}", totalSyntaxErrorScore);
+
+static char? GetFirstIllegalCharacter(string line)
+{
+  var stack = new Stack<char>();
+  foreach (var c in line)
+  {
+    switch (c)
+    {
+      case '(':
+        stack.Push(')');
+        break;
+      case '[':
+        stack.Push(']');
+        break;
+      case '{':
+        stack.Push('}');
+        break;
+      case '<':
+        stack.Push('>');
+        break;
+
+      case ')':
+      case ']':
+      case '}':
+      case '>':
+        if (stack.Count == 0)
+        {
+          // Incomplete line
+          return null;
+        }
+
+        if (stack.Pop() != c)
+        {
+          // Return the first illegal character (corrupted)
+          return c;
+        }
+        break;
+    }
+  }
+
+  // Valid input
+  return null;
+}
