74300383129fe2533160fd0ad642da51947118dd
Yoann Chaudet <yoannchaudet@github.com>
2021-12-11 08:12:17 -0800

    Day 10 part 2

--- 2021/day10/Program.cs
+++ 2021/day10/Program.cs
@@ -5,12 +5,26 @@ var syntaxScores = new Dictionary<char, int>() {
   { '}', 1197 },
   { '>', 25137 },
 };
+var autocompleteScores = new Dictionary<char, int>() {
+  { ')', 1 },
+  { ']', 2 },
+  { '}', 3 },
+  { '>', 4 },
+};
 var totalSyntaxErrorScore = File.ReadAllLines(inputPath)
   .Select(line => GetFirstIllegalCharacter(line))
   .Where(c => c.HasValue)
   .Select(c => syntaxScores[c.Value])
   .Sum();
-Console.WriteLine("Total syntax error score: {0}", totalSyntaxErrorScore);
+Console.WriteLine("Total syntax error score (part 1): {0}", totalSyntaxErrorScore);
+
+var allAutocompleteScores = File.ReadAllLines(inputPath)
+  .Select(line => (line, GetFirstIllegalCharacter(line)))
+  .Where(c => !c.Item2.HasValue)
+  .Select(line => GetAutocompleteScore(line.Item1, autocompleteScores))
+  .ToList();
+allAutocompleteScores.Sort();
+Console.WriteLine("Middle auto complete score (part 2): {0}", allAutocompleteScores[allAutocompleteScores.Count / 2]);
 
 static char? GetFirstIllegalCharacter(string line)
 {
@@ -53,4 +67,42 @@ static char? GetFirstIllegalCharacter(string line)
 
   // Valid input
   return null;
+}
+
+static double GetAutocompleteScore(string line, IDictionary<char, int> autocompleteScores)
+{
+  var stack = new Stack<char>();
+  foreach (var c in line)
+  {
+    switch (c)
+    {
+      case '(':
+        stack.Push(')');
+        break;
+      case '[':
+        stack.Push(']');
+        break;
+      case '{':
+        stack.Push('}');
+        break;
+      case '<':
+        stack.Push('>');
+        break;
+
+      case ')':
+      case ']':
+      case '}':
+      case '>':
+        stack.Pop();
+        break;
+    }
+  }
+
+  double score = 0;
+  while (stack.Count > 0)
+  {
+    score *= 5d;
+    score += autocompleteScores[stack.Pop()];
+  }
+  return score;
 }
