d51e3c85badca4784a1c8b0ee8121315d01e000c
Yoann Chaudet <yoannchaudet@github.com>
2021-12-22 18:31:18 -0800

    Day 18 part 1

--- 2021/day18/Program.cs
+++ 2021/day18/Program.cs
@@ -1,15 +1,31 @@
-﻿var number = new SnailNumberParser("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]").Parse();
-Console.WriteLine(number.ToString());
+﻿var inputPath = "./inputs/input.txt";
+SnailNumber sum = null;
+File.ReadAllLines(inputPath).ToList().ForEach(line =>
+{
+  var number = new SnailNumberParser(line).Parse();
+  if (sum == null)
+  {
+    sum = number;
+  }
+  else
+  {
+    sum += number;
+  }
+});
+Console.WriteLine("Sum: {0}", sum.ToString());
+Console.WriteLine("Magnitude: {0}", sum.GetMagnitude());
 
 class SnailNumberParser
 {
   private string Number;
   private int Position;
+  private IList<SnailNumber> Numbers;
 
   public SnailNumberParser(string number)
   {
     this.Number = number;
     this.Position = 0;
+    this.Numbers = new List<SnailNumber>();
   }
 
   private int ReadLiteral()
@@ -25,18 +41,36 @@ class SnailNumberParser
 
   public SnailNumber Parse()
   {
+    var number = Parse(null);
+
+    // Init most left/right
+    for (var i = 0; i < Numbers.Count; i++)
+    {
+      if (i > 0)
+        Numbers[i].MostLeftNumber = Numbers[i - 1];
+      if (i < Numbers.Count - 1)
+        Numbers[i].MostRightNumber = Numbers[i + 1];
+    }
+
+    return number;
+  }
+
+  private SnailNumber Parse(SnailNumber parent)
+  {
     SnailNumber currentNumber = null;
     while (Position < Number.Length)
     {
       if (Number[Position] == '[')
       {
         Position++;
-        currentNumber = new SnailNumber();
-        currentNumber.Left = Parse();
+        currentNumber = new SnailNumber(parent);
+        currentNumber.Left = Parse(currentNumber);
       }
       else if (char.IsDigit(Number[Position]))
       {
-        return new SnailNumber(ReadLiteral());
+        var n = new SnailNumber(parent, ReadLiteral());
+        Numbers.Add(n);
+        return n;
       }
       else if (Number[Position] == ',')
       {
@@ -45,7 +79,7 @@ class SnailNumberParser
           throw new Exception("Unexpected comma");
         }
         Position++;
-        currentNumber.Right = Parse();
+        currentNumber.Right = Parse(currentNumber);
       }
       else if (Number[Position] == ']')
       {
@@ -68,26 +102,71 @@ class SnailNumberParser
 
 class SnailNumber
 {
+  public SnailNumber? Parent { get; private set; }
   public int? Number { get; set; }
   public SnailNumber? Left { get; set; }
   public SnailNumber? Right { get; set; }
 
-  public SnailNumber()
-  {
+  public SnailNumber? MostLeftNumber { get; set; }
+  public SnailNumber? MostRightNumber { get; set; }
 
+  public SnailNumber(SnailNumber parent)
+  {
+    Parent = parent;
   }
 
-  public SnailNumber(int number)
+  public SnailNumber(SnailNumber parent, int number)
   {
+    Parent = parent;
     Number = number;
   }
 
-  public SnailNumber(SnailNumber left, SnailNumber right)
+  public SnailNumber(SnailNumber parent, SnailNumber left, SnailNumber right)
   {
+    Parent = parent;
     Left = left;
     Right = right;
   }
 
+  // Addition operation
+  public static SnailNumber operator +(SnailNumber a, SnailNumber b)
+  {
+    // NOTE: this is a quick and dirty solution. It cannot adds up the same instance twice and
+    // both Explode and Split don't maintain the most left/right numbers.
+    // Extra parsing is done at each mutation to reset the whole thing.
+
+    var addition = new SnailNumber(null, a, b);
+    addition = new SnailNumberParser(addition.ToString()).Parse();
+
+    // Reduction logic
+    while (true)
+    {
+      // If a pair is nested inside four pairs, explode it
+      var pairAt4 = addition.GetPairAtDepth(4);
+      if (pairAt4 != null)
+      {
+        pairAt4.Explode();
+        addition = new SnailNumberParser(addition.ToString()).Parse();
+        continue;
+      }
+
+      // If a number is 10 or greater, split it
+      var number10OrGreater = addition.GetNumbers().Where(n => n.Number >= 10).FirstOrDefault((SnailNumber)null);
+      if (number10OrGreater != null)
+      {
+        number10OrGreater.Split();
+        addition = new SnailNumberParser(addition.ToString()).Parse();
+        continue;
+      }
+
+      // Done
+      break;
+    }
+
+    return addition;
+  }
+
+  // Return the number's string representation (again)
   public string ToString()
   {
     if (Number.HasValue)
@@ -98,5 +177,110 @@ class SnailNumber
     {
       return "[" + Left.ToString() + "," + Right.ToString() + "]";
     }
+  }
+
+  // Return a pair at a given depth if any.
+  public SnailNumber GetPairAtDepth(int depth)
+  {
+    // Ignore numbers
+    if (Number.HasValue)
+    {
+      return null;
+    }
+
+    // Final depth for a pair
+    if (depth == 0)
+    {
+      return this;
+    }
+
+    // Recursion for a pair
+    else
+    {
+      // Descent left first
+      var left = Left.GetPairAtDepth(depth - 1);
+      if (left != null)
+      {
+        return left;
+      }
+
+      // Then right
+      var right = Right.GetPairAtDepth(depth - 1);
+      if (right != null)
+      {
+        return right;
+      }
+    }
+
+    // Fallback
+    return null;
+  }
+
+  public List<SnailNumber> GetNumbers()
+  {
+    var numbers = new List<SnailNumber>();
+    if (Number.HasValue)
+    {
+      numbers.Add(this);
+    }
+    if (Left != null)
+    {
+      numbers.AddRange(Left.GetNumbers());
+    }
+    if (Right != null)
+    {
+      numbers.AddRange(Right.GetNumbers());
+    }
+    return numbers;
+  }
+
+  // Expode a number.
+  public void Explode()
+  {
+    if (Number.HasValue)
+    {
+      throw new Exception("Cannot explode a number");
+    }
+
+    // Do the additions
+    if (Left.MostLeftNumber != null)
+    {
+      Left.MostLeftNumber.Number += Left.Number;
+    }
+    if (Right.MostRightNumber != null)
+    {
+      Right.MostRightNumber.Number += Right.Number;
+    }
+
+    // Remove the pair
+    Left = null;
+    Right = null;
+    Number = 0;
+  }
+
+  // Split a pair.
+  public void Split()
+  {
+    if (!Number.HasValue)
+    {
+      throw new Exception("Cannot split a pair");
+    }
+
+    var left = (int)Math.Floor(Number.Value / 2d);
+    var right = Number.Value - left;
+
+    Number = null;
+    Left = new SnailNumber(this, left);
+    Right = new SnailNumber(this, right);
+  }
+
+  // Compute the magnitude of the snail number.
+  public int GetMagnitude()
+  {
+    if (Number.HasValue)
+    {
+      return Number.Value;
+    }
+    return 3 * Left.GetMagnitude() + 2 * Right.GetMagnitude();
   }
 }
