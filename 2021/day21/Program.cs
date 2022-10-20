08824ceeee358e9c56b14a247b5805d7e965acc7
Yoann Chaudet <yoannchaudet@github.com>
2022-10-20 12:54:31 -0700

    Not sure what that is

--- 2021/day21/Program.cs
+++ 2021/day21/Program.cs
@@ -1,16 +1,67 @@
-﻿var inputPath = "./inputs/input.txt";
+﻿var inputPath = "./inputs/sample.txt";
 var game = new Game(inputPath, new DeterministicDie(), 1000);
 
 var winner = game.Play();
 var part1 = game.PlayerScores[winner == 0 ? 1 : 0] * game.Die.Rolls;
 Console.WriteLine("Part 1 = {0}", part1);
 
+var sums = new List<int>();
+foreach (var roll1 in new[] { 1, 2, 3 })
+{
+  foreach (var roll2 in new[] { 1, 2, 3 })
+  {
+    foreach (var roll3 in new[] { 1, 2, 3 })
+    {
+      sums.Add(roll1 + roll2 + roll3);
+    }
+  }
+}
+var rolls = sums.GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() }).Select(s => (s.Key, s.Count)).ToArray();
+
+var positions = File.ReadAllLines(inputPath).Select(line => line.Split(':')[1]).Select(int.Parse).Select(x => x - 1).ToArray();
+var wins = (0L, 0L);
+DiracGame((0, 0), (positions[0], positions[1]), (1, 1), ref wins, ref rolls);
+Console.WriteLine("Part 2 = {0}", wins.Item1 > wins.Item2 ? wins.Item1 : wins.Item2);
+
+// Play the Dirac game, return the number of wins for each player
+static void DiracGame((int, int) scores, (int, int) positions, (long, long) universes, ref (long, long) wins, ref (int, int)[] rolls)
+{
+  // Winner
+  if (scores.Item1 >= 21)
+  {
+    wins.Item1 += universes.Item1;
+    return;
+  }
+  if (scores.Item2 >= 21)
+  {
+    wins.Item2 += universes.Item2;
+    return;
+  }
+
+  // Play the game
+  foreach (var rollPlayer1 in rolls)
+  {
+    foreach (var rollPlayer2 in rolls)
+    {
+      var nextPosition = (
+        (positions.Item1 + rollPlayer1.Item1) % 10,
+        (positions.Item2 + rollPlayer2.Item1) % 10);
+      var nextScores = (
+        scores.Item1 + nextPosition.Item1 + 1,
+        scores.Item2 + nextPosition.Item2 + 1);
+      var nextUniverses = (
+        universes.Item1 * rollPlayer1.Item2,
+        universes.Item2 * rollPlayer2.Item2);
+      DiracGame(nextScores, nextPosition, nextUniverses, ref wins, ref rolls);
+    }
+  }
+}
+
 class Game
 {
   public int[] PlayerPositions { get; private set; }
   public int[] PlayerScores { get; private set; }
   public Die Die { get; private set; }
-
   public int MaxScore { get; private set; }
 
   public Game(string inputPath, Die die, int maxScore)
