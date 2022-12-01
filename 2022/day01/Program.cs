d1e3bdd0cbf1facbbe14b417f028297b3401322f
Yoann Chaudet <yoannchaudet@github.com>
2022-11-30 21:15:28 -0800

    day 1

--- 2022/day01/Program.cs
+++ 2022/day01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = "input.txt";
+var caloriesPerElf = GetCaloriesPerElf(input);
+var top1TotalCalories = caloriesPerElf.Max();
+Console.WriteLine("Top 1 total calories = {0}", top1TotalCalories);
+var top3Calories = caloriesPerElf.OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine("Top 3 total calories = {0}", top3Calories);
+
+static List<int> GetCaloriesPerElf(string input)
+{
+  var caloriesPerElf = new List<int>();
+  int currentCalories = 0;
+  foreach (var line in File.ReadAllLines(input))
+  {
+    if (String.IsNullOrEmpty(line))
+    {
+      caloriesPerElf.Add(currentCalories);
+      currentCalories = 0;
+    }
+    else
+    {
+      currentCalories += int.Parse(line);
+    }
+  }
+  // last line
+  caloriesPerElf.Add(currentCalories);
+  return caloriesPerElf;
+}
