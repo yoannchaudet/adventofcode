e37ec3244d2ae40c8cd1b2ce60da58b4b4d3849b
Yoann Chaudet <yoannchaudet@github.com>
2022-12-05 21:27:50 -0800

    Day 6 part 2

--- 2022/day06/Program.cs
+++ 2022/day06/Program.cs
@@ -1,14 +1,23 @@
 ﻿// Part 1
-var firstMarker = GetFirstMarkerPosition(File.ReadAllText("input.txt"));
-Console.WriteLine("Part 1: {0}", firstMarker);
+var buffer = File.ReadAllText("input.txt");
+{
+  var firstPacketMarker = GetFirstMarkerPosition(buffer, 4);
+  Console.WriteLine("Part 1: {0}", firstPacketMarker);
+}
 
+// Part 2
+{
+  var firstMessageMarker = GetFirstMarkerPosition(buffer, 14);
+  Console.WriteLine("Part 1: {0}", firstMessageMarker);
+}
 
 // Return the position of the first marker
-static int GetFirstMarkerPosition(string buffer)
+static int GetFirstMarkerPosition(string buffer, int markerLength = 4)
 {
-  for (int i = Math.Min(4, buffer.Length); i < buffer.Length; i++)
+  for (int i = Math.Min(markerLength, buffer.Length); i < buffer.Length; i++)
   {
-    if (buffer[(i - 4)..i].Distinct().Count() == 4) {
+    if (buffer[(i - markerLength)..i].Distinct().Count() == markerLength)
+    {
       return i;
     }
   }
