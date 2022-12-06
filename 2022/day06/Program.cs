7ea9f7ebb04213bd0ecdbca4992977a9af39e20a
Yoann Chaudet <yoannchaudet@github.com>
2022-12-05 21:17:36 -0800

    Day 6 part 1

---
+++ 2022/day06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Part 1
+var firstMarker = GetFirstMarkerPosition(File.ReadAllText("input.txt"));
+Console.WriteLine("Part 1: {0}", firstMarker);
+
+
+// Return the position of the first marker
+static int GetFirstMarkerPosition(string buffer)
+{
+  for (int i = Math.Min(4, buffer.Length); i < buffer.Length; i++)
+  {
+    if (buffer[(i - 4)..i].Distinct().Count() == 4) {
+      return i;
+    }
+  }
+  return -1;
+}
