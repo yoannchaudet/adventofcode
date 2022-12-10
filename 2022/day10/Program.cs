46cf910f89289c11d78875b4c795bc246665245c
Yoann Chaudet <yoannchaudet@github.com>
2022-12-09 22:01:47 -0800

    day 10 part 2

--- 2022/day10/Program.cs
+++ 2022/day10/Program.cs
@@ -1,4 +1,6 @@
-﻿// Part 1
+﻿var input = "input.txt";
+
+// Part 1
 {
   var x = 1;
   var signalStrength = 0;
@@ -10,7 +12,7 @@
       signalStrength += tick * x;
     }
   });
-  foreach (var instruction in ParseInstructions("input.txt"))
+  foreach (var instruction in ParseInstructions(input))
   {
     if (instruction is AddX)
     {
@@ -24,6 +26,59 @@
     }
   }
   Console.WriteLine("Part 1: {0}", signalStrength);
+}
+
+// Part 2
+{
+  // register
+  var x = 1;
+  // current crt row
+  var crtRow = -1;
+  // crt
+  var crt = new char[][] {
+    "........................................".ToArray(),
+    "........................................".ToArray(),
+    "........................................".ToArray(),
+    "........................................".ToArray(),
+    "........................................".ToArray(),
+    "........................................".ToArray(),
+  };
+  // clock
+  var clock = new Clock(tick =>
+  {
+    var localTick = (tick - 1) % 40;
+    var localX = x % 41;
+
+    // Bump crt row counter
+    if ((tick % 41) - 1 == 0)
+    {
+      crtRow++;
+    }
+
+    // Lit or not?
+    if (Math.Abs(localTick - localX) <= 1)
+    {
+      crt[crtRow][localTick] = '#';
+    }
+  });
+  foreach (var instruction in ParseInstructions(input))
+  {
+    if (instruction is AddX)
+    {
+      clock.Tick();
+      clock.Tick();
+      x += ((AddX)instruction).X;
+    }
+    else if (instruction is Noop)
+    {
+      clock.Tick();
+    }
+  }
+  Console.WriteLine("Part 2:");
+  foreach (var row in crt)
+  {
+    Console.WriteLine(new string(row));
+  }
 }
 
 // Parse instructions
