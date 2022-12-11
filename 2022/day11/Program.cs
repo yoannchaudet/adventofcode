44fcc57d26f8038d0361d28512999980f8258b22
Yoann Chaudet <yoannchaudet@github.com>
2022-12-11 11:49:18 -0800

    day 11 part 2

--- 2022/day11/Program.cs
+++ 2022/day11/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
-using System.Text;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 var input = "input.txt";
 
@@ -45,11 +43,59 @@ var input = "input.txt";
   Console.WriteLine("Part 1: {0}", monkeyBusiness);
 }
 
+// Part 2
+{
+  var monkeys = GetMonkeys(input).ToList();
+  var inspections = new long[monkeys.Count];
+
+  // All divisers are prime (yay!)
+  var totalDivisers = 1l;
+  monkeys.ForEach(m => totalDivisers *= m.Diviser);
+
+  // Run rounds
+  for (var round = 1; round <= 10_000; round++)
+  {
+    for (var i = 0; i < monkeys.Count; i++)
+    {
+      var monkey = monkeys[i];
+
+      // Go through the items the monkey has now
+      foreach (var item in monkey.Items)
+      {
+        var worryLevel = monkey.Operation.Invoke(item);
+
+        // Magic to remain in range
+        worryLevel = worryLevel % totalDivisers;
+
+        if (worryLevel % monkey.Diviser == 0)
+        {
+          monkeys[monkey.DivisibleMonkey].Items.Add(worryLevel);
+        }
+        else
+        {
+          monkeys[monkey.NonDivisibleMonkey].Items.Add(worryLevel);
+        }
+
+        // Increment inspection counter
+        inspections[i]++;
+      }
+
+      // Clear current list
+      monkey.Items.Clear();
+    }
+  }
+
+  // Compute monkey business
+  var top = inspections.OrderByDescending(m => m).Take(2);
+  var monkeyBusiness = top.First() * top.Last();
+  Console.WriteLine("Part 2: {0}", monkeyBusiness);
+}
+
 // Return the monkeys from the input
 static IEnumerable<Monkey> GetMonkeys(string input)
 {
+  var itemCounter = 0;
   var lines = File.ReadAllLines(input);
-  var operationRegex = new Regex(@"  Operation: new = (.+)&");
   for (var i = 0; i < lines.Length; i++)
   {
     var line = lines[i];
