5e085d603bae8096fd5c5c1818836885287c7b67
Yoann Chaudet <yoannchaudet@github.com>
2022-12-13 07:50:28 -0800

    day 13 part 2

--- 2022/day13/Program.cs
+++ 2022/day13/Program.cs
@@ -9,7 +9,7 @@ var input = "input.txt";
   for (var i = 0; i < pairs.Count; i++)
   {
     var pair = pairs[i];
-    if (CompareOrder(pair.Item1, pair.Item2) == -1)
+    if (pair.Item1.CompareTo(pair.Item2) == -1)
     {
       indices.Add(i + 1);
     }
@@ -19,114 +19,117 @@ var input = "input.txt";
 
 // Part 2
 {
-
+  var packets = new List<Packet>();
+  var marker1 = new Packet("[[2]]");
+  var marker2 = new Packet("[[6]]");
+  packets.Add(marker1);
+  packets.Add(marker2);
+  foreach (var tuple in ParseInput(input).ToList())
+  {
+    packets.Add(tuple.Item1);
+    packets.Add(tuple.Item2);
+  }
+  packets.Sort();
+  var decoder = (packets.IndexOf(marker1) + 1) * (packets.IndexOf(marker2) + 1);
+  Console.WriteLine("Part 2: {0}", decoder);
 }
 
-static int CompareOrder(JsonNode left, JsonNode right)
-{
-  // Compare two ints
-  if (left is JsonValue leftNumber && right is JsonValue rightNumber)
-  {
-    var leftValue = leftNumber.GetValue<int>();
-    var rightValue = rightNumber.GetValue<int>();
-    return leftValue.CompareTo(rightValue);
-  }
-
-  // 2 arrays
-  else if (left is JsonArray leftArray && right is JsonArray rightArray)
-  {
-    int? lastOrder = null;
-    for (var i = 0; i < Math.Min(leftArray.Count, rightArray.Count); i++)
-    {
-      lastOrder = CompareOrder(leftArray[i], rightArray[i]);
-
-      // Stop if a decision is made
-      if (lastOrder != 0)
-      {
-        return lastOrder.Value;
-      }
-    }
-
-    // No decision
-    if (lastOrder == null || lastOrder == 0)
-    {
-      // Left run out
-      if (rightArray.Count > leftArray.Count)
-      {
-        return -1;
-      }
-
-      // Right run out
-      else if (rightArray.Count < leftArray.Count)
-      {
-        return 1;
-      }
-
-      // No decision
-      else
-      {
-        return 0;
-      }
-    }
-
-    // Return decision
-    return lastOrder.Value;
-  }
-
-  // 1 array / 1 number
-  else
-  {
-    return CompareOrder(ToArray(left), ToArray(right));
-  }
-}
-
-// Return a JsonNode as an array
-static JsonArray ToArray(JsonNode node)
-{
-  if (node is JsonArray array)
-  {
-    return array;
-  }
-
-  return new JsonArray() { JsonNode.Parse(node.ToJsonString()) };
-}
-
-static IEnumerable<Tuple<JsonArray, JsonArray>> ParseInput(string input)
+static IEnumerable<Tuple<Packet, Packet>> ParseInput(string input)
 {
   var lines = File.ReadAllLines(input);
   for (var i = 0; i < lines.Length; i++)
   {
-    yield return Tuple.Create(ParsePacket(lines[i]), ParsePacket(lines[++i]));
+    yield return Tuple.Create(new Packet(lines[i]), new Packet(lines[++i]));
     i++;
   }
 }
 
-// Deserialize a single packet
-static JsonArray ParsePacket(string line)
+
+class Packet : IComparable<Packet>
 {
-  var node = JsonNode.Parse(line);
-  if (node is JsonArray array)
+  public JsonArray Value { get; private set; }
+
+  public Packet(string input)
   {
-    return array;
-  }
-  throw new Exception("Invalid packet: " + line);
-}
-
-class Packet
-{
-  public List<Packet> Values { get; private set; }
-
-  public int? Value { get; private set; }
-
-  public Packet(int value)
-  {
+    var value = JsonNode.Parse(input) as JsonArray;
+    if (value == null)
+    {
+      throw new Exception("Invalid packet: " + input);
+    }
     Value = value;
-    Values = null;
   }
 
-  public Packet(Packet parent, IEnumerable<Packet> values)
+  public int CompareTo(Packet? other)
   {
-    Value = null;
-    Values = values.ToList();
+    return CompareTo(Value, other.Value);
+  }
+
+  static int CompareTo(JsonNode left, JsonNode right)
+  {
+    // Compare two ints
+    if (left is JsonValue leftNumber && right is JsonValue rightNumber)
+    {
+      var leftValue = leftNumber.GetValue<int>();
+      var rightValue = rightNumber.GetValue<int>();
+      return leftValue.CompareTo(rightValue);
+    }
+
+    // 2 arrays
+    else if (left is JsonArray leftArray && right is JsonArray rightArray)
+    {
+      int? lastOrder = null;
+      for (var i = 0; i < Math.Min(leftArray.Count, rightArray.Count); i++)
+      {
+        lastOrder = CompareTo(leftArray[i], rightArray[i]);
+
+        // Stop if a decision is made
+        if (lastOrder != 0)
+        {
+          return lastOrder.Value;
+        }
+      }
+
+      // No decision
+      if (lastOrder == null || lastOrder == 0)
+      {
+        // Left run out
+        if (rightArray.Count > leftArray.Count)
+        {
+          return -1;
+        }
+
+        // Right run out
+        else if (rightArray.Count < leftArray.Count)
+        {
+          return 1;
+        }
+
+        // No decision
+        else
+        {
+          return 0;
+        }
+      }
+
+      // Return decision
+      return lastOrder.Value;
+    }
+
+    // 1 array / 1 number
+    else
+    {
+      return CompareTo(ToArray(left), ToArray(right));
+    }
+  }
+
+  // Return a JsonNode as an array
+  static JsonArray ToArray(JsonNode node)
+  {
+    if (node is JsonArray array)
+    {
+      return array;
+    }
+
+    return new JsonArray() { JsonNode.Parse(node.ToJsonString()) };
   }
 }
