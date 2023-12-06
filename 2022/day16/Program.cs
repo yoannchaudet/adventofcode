8b9c794132117ca837237dbf44a170def8fff1ea
Yoann Chaudet <c.yoann@gmail.com>
2023-12-06 17:15:19 +0800

    day 5

--- 2022/day16/Program.cs
+++ 2022/day16/Program.cs
@@ -1,134 +1,33 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections;
+using System.Text.RegularExpressions;
 
-var valves = GetValves("input.txt");
-
-// Compute distance between all valves
-var valveDistance = new Dictionary<string, Dictionary<string, int>>();
-foreach (var valve in valves.Values)
-{
-  var (distance, _) = BellmanFord(valves, valve.Label);
-  valveDistance.Add(valve.Label, distance);
-}
-
-var promisingOrders = GetPromisingValveOrders(valves, valveDistance, 30, new List<string>()).ToList();
-var pressureRelease = promisingOrders.Select(p => (p, GetPressureRelease(valves, p, valveDistance))).MaxBy(p => p.Item2);
-Console.WriteLine("Part 1: {0}", pressureRelease.Item2);
-
-// Return a list of promising valve orders
-static IEnumerable<List<string>> GetPromisingValveOrders(Dictionary<string, Valve> allValves, Dictionary<string, Dictionary<string, int>> allValvesDistances, int minutesLeft, List<string> selectedValves)
-{
-  // Get current valve (either starting point or last one in the chain)
-  var currentValve = selectedValves.Count == 0 ? "AA" : selectedValves.Last();
-
-  // Get valves to consider
-  var valvesToConsider = allValves.Keys.Where(v => !selectedValves.Contains(v) && allValves[v].FlowRate > 0).ToList();
-  if (valvesToConsider.Count > 0)
-  {
-    int top = selectedValves.Count < 5 ? 3 : 1;
-    var topN = valvesToConsider.Select(v => (v, (minutesLeft - allValvesDistances[currentValve][v] - 1) * allValves[v].FlowRate)).OrderByDescending(v => v.Item2).Take(top).ToList();
-    foreach (var valve in topN)
-    {
-      var newSelectedValves = new List<string>(selectedValves);
-      newSelectedValves.Add(valve.Item1);
-      foreach (var order in GetPromisingValveOrders(allValves, allValvesDistances, (minutesLeft - allValvesDistances[currentValve][valve.Item1] - 1), newSelectedValves))
-      {
-        yield return order;
-      }
-    }
-  }
-
-  yield return selectedValves;
-}
-
-// Return the list of permutations of a given list
-static IEnumerable<List<string>> GetPermutations(List<string> list)
-{
-  if (list.Count == 1) return new List<List<string>> { list };
-  var permutations = new List<List<string>>();
-  for (int i = 0; i < list.Count; i++)
-  {
-    var smallerPermutations = GetPermutations(list.Where((val, index) => index != i).ToList());
-    foreach (var smallerPermutation in smallerPermutations)
-    {
-      var permutation = new List<string> { list[i] };
-      permutation.AddRange(smallerPermutation);
-      permutations.Add(permutation);
-    }
-  }
-  return permutations;
-}
-
-static int GetPressureRelease(Dictionary<string, Valve> valves, List<string> valvesToOpen, Dictionary<string, Dictionary<string, int>> valveDistance)
-{
-  var currentLocation = valves["AA"];
-  var pressureRate = 0;
-  var pressureRelease = 0;
-  var minutes = 1;
-  var nextValveIndex = 0;
-  var stop = false;
-  while (minutes <= 30)
-  {
-    // select valves that can be opened
-    if (!stop && nextValveIndex <= valvesToOpen.Count() - 1)
-    {
-      var nextValve = valvesToOpen[nextValveIndex++];
-      var cost = valveDistance[currentLocation.Label][nextValve];
-
-      if (minutes + cost < 30)
-      {
-        // move
-        minutes += cost;
-        pressureRelease += pressureRate * cost;
-
-        // open valve
-        minutes++;
-        pressureRelease += pressureRate;
-        pressureRate += valves[nextValve].FlowRate;
-        currentLocation = valves[nextValve];
-      }
-      else
-      {
-        stop = true;
-      }
-    }
-
-    // let time tick
-    else
-    {
-      minutes++;
-      pressureRelease += pressureRate;
-    }
-  }
-  return pressureRelease;
-}
-
-// get distance, predecessor maps between all valves and a given source
-static (Dictionary<string, int>, Dictionary<string, string>) BellmanFord(Dictionary<string, Valve> valves, string source)
-{
-  var distance = new Dictionary<string, int>();
-  var predecessor = new Dictionary<string, string>();
-  foreach (var valve in valves.Values)
-  {
-    distance.Add(valve.Label, Int16.MaxValue);
-    predecessor.Add(valve.Label, null);
-  }
-  distance[source] = 0;
-  for (int i = 0; i < valves.Count - 1; i++)
-  {
-    foreach (var valve in valves.Values)
-    {
-      foreach (var adjacentValve in valve.AdjacentValves)
-      {
-        if (distance[valve.Label] + 1 < distance[adjacentValve.Label])
-        {
-          distance[adjacentValve.Label] = distance[valve.Label] + 1;
-          predecessor[adjacentValve.Label] = valve.Label;
-        }
-      }
-    }
-  }
-  return (distance, predecessor);
-}
+var valves = GetValves("inputs/sample.txt");
+var steps = new Steps();
+steps.Move("DD");
+steps.OpenValve("DD", valves["DD"].FlowRate);
+steps.Move("CC");
+steps.Move("BB");
+steps.OpenValve("BB", valves["BB"].FlowRate);
+steps.Move("AA");
+steps.Move("II");
+steps.Move("JJ");
+steps.OpenValve("JJ", valves["JJ"].FlowRate);
+steps.Move("II");
+steps.Move("AA");
+steps.Move("DD");
+steps.Move("EE");
+steps.Move("FF");
+steps.Move("GG");
+steps.Move("HH");
+steps.OpenValve("HH", valves["HH"].FlowRate);
+steps.Move("GG");
+steps.Move("FF");
+steps.Move("EE");
+steps.OpenValve("EE", valves["EE"].FlowRate);
+steps.Move("DD");
+steps.Move("CC");
+steps.OpenValve("CC", valves["CC"].FlowRate);
+Console.WriteLine(steps);
 
 // Parse the input
 static Dictionary<string, Valve> GetValves(string input)
@@ -144,7 +43,7 @@ static Dictionary<string, Valve> GetValves(string input)
     }
 
     var label = match.Groups[1].Value.Trim();
-    var valve = valves.ContainsKey(label) ? valves[label] : new Valve(label);
+    var valve = valves.TryGetValue(label, out var valve1) ? valve1 : new Valve(label);
     valve.FlowRate = int.Parse(match.Groups[2].Value);
     if (!valves.ContainsKey(valve.Label)) valves.Add(label, valve);
     foreach (var adjacentValve in match.Groups[3].Value.Split(','))
@@ -174,3 +73,44 @@ class Valve
     AdjacentValves = new List<Valve>();
   }
 }
+
+class Steps
+{
+  public List<string> Path { get; init; }
+  private int MaxSteps { get; init; }
+  public int FlowRate { get; private set; }
+
+  public Steps(int maxSteps = 30)
+  {
+    Path = new List<string>();
+    MaxSteps = 30;
+    FlowRate = 0;
+  }
+  
+  public void Move(string valve)
+  {
+    if (!IsComplete())
+      Path.Add($"Move to {valve}");
+  }
+
+  public void OpenValve(string valve, int flowRate)
+  {
+    if (!IsComplete())
+    {
+      Path.Add($"Open valve {valve}");
+      var remainingSteps = MaxSteps - Path.Count;
+      if (remainingSteps > 0)
+        FlowRate += remainingSteps * flowRate;
+    }
+  }
+  
+  public bool IsComplete()
+  {
+    return Path.Count() >= MaxSteps;
+  }
+  
+  public override string ToString()
+  {
+    return string.Join(System.Environment.NewLine, Path) + System.Environment.NewLine + $"Total flow rate = {FlowRate}";
+  }
+} 
---
+++ 2023/day05/Program.cs
@@ -0,0 +1,200 @@
+﻿const string input = "inputs/input.txt";
+var (seeds, maps) = ParseInput(input);
+
+// Part 1
+var part1 = seeds.Min(seed => GetLocation(seed, maps));
+Console.WriteLine($"Part 1 = {part1}");
+
+// Part 2
+
+// Extract seed ranges
+var seedRanges = new List<Range>();
+for (var i = 0; i < seeds.Count; i += 2)
+    seedRanges.Add(new Range(seeds[i], seeds[i] + seeds[i + 1] - 1));
+Console.WriteLine("Seed ranges:");
+Console.WriteLine(string.Join(Environment.NewLine, seedRanges));
+
+// Start from an arbitrary large number... (this is cheating and just iterate over locations until we find a matching seed)
+for (var i = 1_000_000L; i < long.MaxValue; i += 100)
+{
+    var possibleLocations = new List<long>(capacity: 100);
+    Parallel.For(i, i + 100, location =>
+    {
+        var seed = GetSeed(location, maps);
+        if (seedRanges.Any(r => r.Contains(seed)))
+        {
+            possibleLocations.Add(location);
+        }
+    });
+    if (possibleLocations.Count > 0)
+    {
+        var part2 = possibleLocations.Min();
+        Console.WriteLine("Part 2 = " + part2);
+        return;
+    }
+}
+
+long GetLocation(long seed, Dictionary<string, Map> maps)
+{
+    var source = "seed";
+    var currentValue = seed;
+    while (maps.ContainsKey(source) && source != "location")
+    {
+        var matchingRanges = maps[source].Ranges.FirstOrDefault(ranges => ranges.SourceRange.Contains(currentValue));
+        if (matchingRanges != null) currentValue = matchingRanges.ConvertSourceToDestination(currentValue);
+        source = maps[source].Destination;
+    }
+
+    return currentValue;
+}
+
+long GetSeed(long location, Dictionary<string, Map> maps)
+{
+    var destination = "location";
+    var currentValue = location;
+    do
+    {
+        var map = maps.Values.FirstOrDefault(m => m.Destination == destination);
+        var matchinRanges = map.Ranges.FirstOrDefault(ranges => ranges.DestinationRange.Contains(currentValue));
+        if (matchinRanges != null)
+        {
+            currentValue = matchinRanges.ConvertDestinationToSource(currentValue);
+        }
+
+        destination = map.Source;
+    } while (destination != "seed");
+
+    return currentValue;
+}
+
+(List<long>, Dictionary<string, Map>) ParseInput(string inputFile)
+{
+    var seeds = new List<long>();
+    var maps = new Dictionary<string, Map>();
+    Map? currentMap = null;
+
+    foreach (var line in File.ReadAllLines(inputFile))
+        // Extract seeds
+        if (line.StartsWith("seeds:"))
+        {
+            seeds = line.Substring("seeds: ".Length).Split(" ").Select(s => s.Trim()).Select(long.Parse).ToList();
+        }
+
+        // Start extracting map
+        else if (line.Contains("map:"))
+        {
+            var name = line.Split(" ").First().Split("-to-");
+            currentMap = new Map(name.First(), name.Last(), new List<Ranges>());
+        }
+
+        // Last line or empty line (lazy here, let's tweak the input)
+        else if (string.IsNullOrWhiteSpace(line))
+        {
+            if (currentMap != null)
+            {
+                maps.Add(currentMap.Source, currentMap);
+                currentMap = null;
+            }
+        }
+
+        // Map line
+        else
+        {
+            if (currentMap != null)
+            {
+                var range = line.Split(" ").Select(s => s.Trim()).Select(long.Parse).ToList();
+                currentMap.Ranges.Add(new Ranges(range[0], range[1], range[2]));
+            }
+        }
+
+    return (seeds, maps);
+}
+
+internal class Map
+{
+    public Map(string source, string destination, List<Ranges> ranges)
+    {
+        Source = source;
+        Destination = destination;
+        Ranges = ranges;
+    }
+
+    public string Source { get; init; }
+    public string Destination { get; init; }
+    public List<Ranges> Ranges { get; init; }
+}
+
+internal class Ranges
+{
+    public Ranges(long destinationRangeStart, long sourceRangeStart, long rangeLength)
+    {
+        SourceRange = new Range(sourceRangeStart, sourceRangeStart + rangeLength - 1);
+        DestinationRange = new Range(destinationRangeStart, destinationRangeStart + rangeLength - 1);
+    }
+
+    public Ranges(Range source, Range destination)
+    {
+        SourceRange = source;
+        DestinationRange = destination;
+    }
+
+    public Range? SourceRange { get; init; }
+
+    public Range DestinationRange { get; init; }
+
+    public long ConvertSourceToDestination(long source)
+    {
+        if (SourceRange != null && SourceRange.Contains(source))
+        {
+            var delta = source - SourceRange.Start;
+            return DestinationRange.Start + delta;
+        }
+
+        return source;
+    }
+
+    public long ConvertDestinationToSource(long destination)
+    {
+        if (SourceRange != null)
+        {
+            var delta = destination - DestinationRange.Start;
+            return SourceRange.Start + delta;
+        }
+
+        return destination;
+    }
+}
+
+internal class Range
+{
+    public Range(long start, long end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public long Start { get; init; }
+    public long End { get; init; }
+
+    public bool Contains(long value)
+    {
+        return value >= Start && value <= End;
+    }
+
+    public Range? GetOverlap(Range range)
+    {
+        if (range.End < Start || range.Start > End) return null;
+        return new Range(Math.Min(Start, range.Start), Math.Max(End, range.End));
+    }
+
+    public Range? GetIntersection(Range range)
+    {
+        if (range.End < Start || range.Start > End) return null;
+        return new Range(Math.Max(Start, range.Start), Math.Min(End, range.End));
+    }
+
+    public override string ToString()
+    {
+        return $"[{Start}; {End}] (size {End - Start})";
+    }
+}
