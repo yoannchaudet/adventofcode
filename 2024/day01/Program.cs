89d75f2792f8b1eff4be395a222d731229a7c5bb
Yoann Chaudet <c.yoann@gmail.com>
2024-12-03 09:26:26 -0800

    day1+day2

--- 2024/day01/Program.cs
+++ 2024/day01/Program.cs
@@ -2,27 +2,21 @@
 var l1 = new List<int>();
 var l2 = new List<int>();
 foreach (var line in File.ReadLines(input))
-{ 
+{
     var ids = line.Split("  ");
     l1.Add(int.Parse(ids[0]));
     l2.Add(int.Parse(ids[1]));
-};
+}
 l1.Sort();
 l2.Sort();
 
 // Part 1
 var part1 = 0;
-for (var i = 0; i < l1.Count; i++)
-{
-    part1 += Math.Abs(l2[i] - l1[i]);
-}
+for (var i = 0; i < l1.Count; i++) part1 += Math.Abs(l2[i] - l1[i]);
 Console.WriteLine($"Part 1 = {part1}");
 
 // Part 2
 var part2 = 0;
-for (var i = 0; i < l1.Count; i++)
-{
-    part2 += l1[i] * l2.Count(n => n == l1[i]);
-}
+foreach (var t in l1)
+    part2 += t * l2.Count(n => n == t);
 Console.WriteLine($"Part 2 = {part2}");
-
---
+++ 2024/day02/Program.cs
@@ -0,0 +1,44 @@
+﻿var input = "i_puzzle.txt";
+var reports = File.ReadLines(input).Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToList();
+
+var part1 = reports.Count(r => IsSafe(r) == -1);
+Console.WriteLine($"Part 1: {part1}");
+var part2 = reports.Count(IsSafePart2);
+Console.WriteLine($"Part 2: {part2}");
+
+int IsSafe(int[] levels)
+{
+    // assume more than 1 level
+    var increasing = levels[0] < levels[1];
+    for (var i = 1; i < levels.Length; i++)
+    {
+        // Check direction
+        if (increasing && levels[i - 1] > levels[i]) return i;
+        if (!increasing && levels[i - 1] < levels[i]) return i;
+
+        // Check distance
+        var dist = Math.Abs(levels[i - 1] - levels[i]);
+        if (dist is > 3 or < 1)
+            return i;
+    }
+
+    return -1;
+}
+
+bool IsSafePart2(int[] levels)
+{
+    var isSafe = IsSafe(levels);
+    if (isSafe == -1)
+        return true;
+
+    // Check again but removing 1 level each time
+    for (var i = 0; i < levels.Length; i++)
+    {
+        var newLevel = levels
+            .Where((value, index) => index != i)
+            .ToArray();
+        if (IsSafe(newLevel) == -1) return true;
+    }
+
+    return false;
+}
