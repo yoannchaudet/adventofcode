53bf79cb45e22b66a886e815d77decce58c3144d
Yoann Chaudet <c.yoann@gmail.com>
2024-12-08 22:24:31 -0800

    day 9 part 2

--- 2024/day09/BlockOrganizer.cs
+++ 2024/day09/BlockOrganizer.cs
@@ -2,54 +2,119 @@ namespace day09;
 
 public class BlockOrganizer
 {
-    public List<long> Blocks { get; init; }
-    
     public BlockOrganizer(string diskMap)
     {
         // Init blocks
         Blocks = new List<long>();
-        bool nextBlocksFree = false;
-        int blockId = 0;
+        var nextBlocksFree = false;
+        var blockId = 0;
         foreach (var b in diskMap)
         {
-            long blockCount = long.Parse(b.ToString());
-            for (var j = 0; j < blockCount; j++)
-            {
-                Blocks.Add(nextBlocksFree ? -1 : blockId);
-            }
-            
-            if (!nextBlocksFree)
-                blockId++;
+            var blockCount = long.Parse(b.ToString());
+            for (var j = 0; j < blockCount; j++) Blocks.Add(nextBlocksFree ? -1 : blockId);
+
+            if (!nextBlocksFree) blockId++;
             nextBlocksFree = !nextBlocksFree;
         }
     }
 
-    public void Compact()
+    private List<long> Blocks { get; init; }
+
+    public void CompactPart1()
     {
         for (var currentBlockId = Blocks.Count - 1; currentBlockId >= 0; currentBlockId--)
         {
-            var b = Blocks[currentBlockId];
-            
-            // Skip free blocks
-            if (b == -1)
-                continue;
+            var block = Blocks[currentBlockId];
 
+            // Skip free blocks
+            if (block == -1) continue;
+
+            // Find the first free block
             var firstFreeBlockId = Blocks.IndexOf(-1);
+
             // No more free blocks / stop
-            if (firstFreeBlockId == -1)
-                break;
+            if (firstFreeBlockId == -1) break;
             // Reached the end
-            if (firstFreeBlockId >= currentBlockId)
-                break;
-            
+            if (firstFreeBlockId >= currentBlockId) break;
+
             // Move
-            Blocks[firstFreeBlockId] = b;
+            Blocks[firstFreeBlockId] = block;
             Blocks[currentBlockId] = -1;
+        }
+    }
+
+    public void CompactPart2()
+    {
+        for (var currentBlockId = Blocks.Count - 1; currentBlockId >= 0; currentBlockId--)
+        {
+            var block = Blocks[currentBlockId];
+            var blockLength = RepeatingBlocksBackward(currentBlockId);
+
+            // Skip free blocks
+            if (block == -1) continue;
+
+            // Find the first free block
+            var firstFreeBlockId = GetFirstFreeBlockId(blockLength);
+
+            // No more free blocks / stop
+            if (!(firstFreeBlockId == -1 || firstFreeBlockId >= currentBlockId))
+                // Move
+                for (var i = 0; i < blockLength; i++)
+                {
+                    Blocks[firstFreeBlockId + i] = block;
+                    Blocks[currentBlockId - i] = -1;
+                }
+
+            currentBlockId -= blockLength - 1;
         }
     }
 
     public long ComputeChecksum()
     {
-        return Blocks.Where(b => b != -1).Select((block, fileId) => fileId * block ).Sum();
+        return Blocks.Select((block, fileId) => block == -1 ? 0 : fileId * block).Sum();
+    }
+
+    private int GetFirstFreeBlockId(int minimumLength)
+    {
+        for (var i = 0; i < Blocks.Count; i++)
+            if (Blocks[i] == -1)
+            {
+                var blockLength = RepeatingBlocksForward(i);
+                if (blockLength >= minimumLength)
+                    return i;
+                i += blockLength - 1;
+            }
+
+        return -1;
+    }
+
+    // Return the number of repeating blocks starting at given index and going forward
+    private int RepeatingBlocksForward(int blockIndex)
+    {
+        if (blockIndex == -1) return 0;
+        var block = Blocks[blockIndex];
+        var repeatingBlocks = 1;
+        for (var i = blockIndex + 1; i < Blocks.Count; i++)
+        {
+            if (Blocks[i] != block) break;
+            repeatingBlocks++;
+        }
+
+        return repeatingBlocks;
+    }
+
+    // Return the number of repeating blocks starting at given index and going forward
+    private int RepeatingBlocksBackward(int blockIndex)
+    {
+        if (blockIndex == -1) return 0;
+        var block = Blocks[blockIndex];
+        var repeatingBlocks = 1;
+        for (var i = blockIndex - 1; i >= 0; i--)
+        {
+            if (Blocks[i] != block) break;
+            repeatingBlocks++;
+        }
+
+        return repeatingBlocks;
     }
 }
--- 2024/day09/Program.cs
+++ 2024/day09/Program.cs
@@ -2,7 +2,19 @@
 
 var input = "i_puzzle.txt";
 var diskMap = File.ReadLines(input).FirstOrDefault("");
-var bo = new BlockOrganizer(diskMap);
-bo.Compact();
-var part1 = bo.ComputeChecksum();
-Console.WriteLine($"Part 1 = {part1}");
+
+// Part 1
+{
+    var bo = new BlockOrganizer(diskMap);
+    bo.CompactPart1();
+    var part1 = bo.ComputeChecksum();
+    Console.WriteLine($"Part 1 = {part1}");
+}
+
+// Part 2
+{
+    var bo = new BlockOrganizer(diskMap);
+    bo.CompactPart2();
+    var part2 = bo.ComputeChecksum();
+    Console.WriteLine($"Part 2 = {part2}");
+}
