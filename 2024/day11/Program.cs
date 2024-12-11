8cf1751bc95c5e0a6514f60b6cf6a1ddbfd1d980
Yoann Chaudet <c.yoann@gmail.com>
2024-12-10 22:18:06 -0800

    smarter solution

--- 2024/day11/Program.cs
+++ 2024/day11/Program.cs
@@ -3,16 +3,16 @@ using day11;
 
 var input = "i_puzzle.txt";
 var sa = new StoneAnalyzer(input);
-var part1 = sa.GetStonesAfterBlinks(26);
+var part1 = sa.GetStonesAfterBlinks(3);
 
 // Part 1
 Console.WriteLine($"Part 1 = {part1}");
 
 // Part 2
-Console.WriteLine("Starting part 2...");
+Console.WriteLine("Starting part 2 stopwatch...");
 var sw = new Stopwatch();
 sw.Start();
 var part2 = sa.GetStonesAfterBlinks(75);
 Console.WriteLine($"Part 2 = {part2}");
 sw.Stop();
-Console.WriteLine("Elapsed = {sw.Elapsed}");
+Console.WriteLine($"Elapsed = {sw.Elapsed.ToString()}");
--- 2024/day11/StoneAnalyzer.cs
+++ 2024/day11/StoneAnalyzer.cs
@@ -2,57 +2,68 @@ namespace day11;
 
 public class StoneAnalyzer
 {
-    public List<long> Stones { get; }
+    private Dictionary<(long, int), long> _cache = new();
 
     public StoneAnalyzer(string input)
     {
         Stones = File.ReadLines(input).FirstOrDefault("").Split(" ").Select(long.Parse).ToList();
     }
-    
+
+    private List<long> Stones { get; }
+
     public long GetStonesAfterBlinks(int blinkCount)
     {
+        _cache.Clear();
         long stones = 0;
-        foreach (var stone in Stones)
-        {
-            stones += GetStonesCountAfterBlinks(stone, blinkCount);
-        }
+        foreach (var stone in Stones) stones += GetStonesCountAfterBlinks(stone, blinkCount);
         return stones;
     }
-    
-    // For part 2, let's get recursive
+
+    // Rewritten recursive for part 2, but with a cache to speed up the resolution
     public long GetStonesCountAfterBlinks(long stone, int blinkCount)
     {
         // Exit
-        if (blinkCount == 0)
+        if (blinkCount == 0) return 1;
+
+        // Check cache
+        var key = (stone, blinkCount);
+        if (_cache.TryGetValue(key, out var blinks))
+            return blinks;
+
+        if (stone == 0)
         {
-            return 1;
+            var next = GetStonesCountAfterBlinks(1, blinkCount - 1);
+            _cache.Add(key, next);
+            return next;
         }
-        
-        if (stone == 0 )
-        {
-            return GetStonesCountAfterBlinks(1, blinkCount - 1);
-        }
-        
+
         var digits = Digits(stone);
         if (digits % 2 == 0)
         {
             var (left, right) = Split(stone, digits);
-            return GetStonesCountAfterBlinks(left, blinkCount - 1) + GetStonesCountAfterBlinks(right, blinkCount - 1);
+            var next = GetStonesCountAfterBlinks(left, blinkCount - 1) +
+                       GetStonesCountAfterBlinks(right, blinkCount - 1);
+            _cache.Add(key, next);
+            return next;
         }
 
-        return GetStonesCountAfterBlinks(stone * 2024, blinkCount - 1);
-    }
-    
-    public static long Digits(long number)
-    {
-        return (long) Math.Floor( Math.Log10(number)) + 1;
+        {
+            var next = GetStonesCountAfterBlinks(stone * 2024, blinkCount - 1);
+            _cache.Add(key, next);
+            return next;
+        }
     }
 
-    public static (long, long) Split(long number, long digits)
+    private static long Digits(long number)
+    {
+        return (long)Math.Floor(Math.Log10(number)) + 1;
+    }
+
+    private static (long, long) Split(long number, long digits)
     {
         var scale = digits / 2;
-        var left = (long) (number /  Math.Pow(10, scale));
-        var right = (long) (number % Math.Pow(10, scale));
+        var left = (long)(number / Math.Pow(10, scale));
+        var right = (long)(number % Math.Pow(10, scale));
         return (left, right);
     }
 }
