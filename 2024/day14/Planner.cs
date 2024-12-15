27a2ca6273fcb3a45d88d79b18e083c159e74fdd
Yoann Chaudet <c.yoann@gmail.com>
2024-12-14 17:36:19 -0800

    day 14 part 1

---
+++ 2024/day14/Planner.cs
@@ -0,0 +1,125 @@
+using System.Text.RegularExpressions;
+
+namespace day14;
+
+// All coordinate tuples are x, y
+
+public class Planner
+{
+    public (int, int) Space { get; }
+    
+    public List<Robot> Robots { get; }
+
+    public int[][] RobotsPerPosition { get; private set; }
+    
+    public Planner((int,int) space, string input)
+    {
+        Space = space;
+        RobotsPerPosition = new int[space.Item2][];
+        for (var i = 0; i < space.Item2; i++)
+        {
+            RobotsPerPosition[i] = new int[space.Item1];
+        }
+        
+        // Parse input
+        var regex = new Regex(@"p\=([0-9]+)\,([0-9]+) v\=(\-?[0-9]+)\,(\-?[0-9]+)");
+        Robots = File.ReadLines(input).Select(line =>
+        {
+            var match = regex.Match(line);
+            var position = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+            var velocity = (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+            return new Robot(position, velocity);
+        }).ToList();
+    }
+
+    // Make all robots move one step
+    public void Tick()
+    {
+        // Move all robots
+        foreach (var robot in Robots)
+        {
+            var (x,y) = robot.Position;
+            var (vx,vy) = robot.Velocity;
+        
+            robot.Position = (Bound(x + vx, Space.Item1), Bound(y + vy, Space.Item2));;
+        }   
+        
+        // Recompute map
+        for (var y = 0; y < Space.Item2; y++)
+        {
+            for (var x = 0; x < Space.Item1; x++)
+            {
+                var robots = Robots.Count(r => r.Position == (x, y));
+                RobotsPerPosition[y][x] = robots;
+            }
+        }
+    }
+
+    public int GetSafetyFactor()
+    {
+        // 0 1
+        // 2 3
+        var quadrants = new int[] { 0,0,0,0};
+        
+        var yy = Space.Item2 / 2;
+        var xx = Space.Item1 / 2;
+
+        for (var y = 0; y < RobotsPerPosition.Length; y++)
+        {
+            for (var x = 0; x < RobotsPerPosition[y].Length; x++)
+            {
+                if (y < yy && x < xx)
+                    quadrants[0] += RobotsPerPosition[y][x];
+                if (y < yy && x > xx)
+                    quadrants[1] += RobotsPerPosition[y][x];
+                if (y > yy && x < xx)
+                    quadrants[2] += RobotsPerPosition[y][x];
+                if (y > yy && x > xx)
+                    quadrants[3] += RobotsPerPosition[y][x];
+            }
+        }
+
+        return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]; 
+    }
+
+    private int Bound(int value, int dimension)
+    {
+        if (value >= dimension)
+        {
+            value %= dimension;
+        }
+
+        if (value < 0)
+        {
+            value = (dimension + value) % dimension;
+        }
+
+        return value;
+    }
+
+    public void PrintMap()
+    {
+        for (var y = 0; y < Space.Item2; y++)
+        {
+            for (var x = 0; x < Space.Item1; x++)
+            {
+                var robots = RobotsPerPosition[y][x];
+                Console.Write( robots == 0 ? "." : (robots > 9 ?"#" :  robots));
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    
+    public class Robot
+    {
+        public (int, int) Position { get; set; }
+        public (int,int) Velocity { get; }
+        
+        public Robot((int, int) position, (int,int) velocity)
+        {
+            Position = position;
+            Velocity = velocity;
+        }
+    }
+}
---
+++ 2024/day14/Program.cs
@@ -0,0 +1,11 @@
+﻿using day14;
+
+// var planner = new Planner((11, 7), "i_sample.txt");
+var planner = new Planner((101, 103), "i_puzzle.txt");
+
+// Part 1
+planner.PrintMap();
+for (var tick = 0; tick < 100; tick++)
+    planner.Tick();
+planner.PrintMap();
+Console.WriteLine($"Part 1 = {planner.GetSafetyFactor()}");
