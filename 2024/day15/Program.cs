9850187ed9347c2a09c7536785afeec903d96fb8
Yoann Chaudet <c.yoann@gmail.com>
2024-12-15 16:58:39 -0800

    day 15 part 2

--- 2024/day15/Program.cs
+++ 2024/day15/Program.cs
@@ -1,11 +1,21 @@
 ﻿using day15;
 
 var input = "i_puzzle.txt";
-var (sokoban, directions) = Sokoban.ParseInput(input);
 
-sokoban.PrintMap();
-foreach (var direction in directions)
-    // Console.WriteLine($"Moving in direction {direction}");
-    sokoban.Move(direction);
-// sokoban.PrintMap();
-Console.WriteLine($"Part 1 = {sokoban.GetSumGPS()}");
+// Part 1
+{
+    var (sokoban, directions) = Sokoban.ParseInput(input);
+    sokoban.PrintMap();
+    foreach (var direction in directions)
+        sokoban.Move(direction);
+    Console.WriteLine($"Part 1 = {sokoban.GetSumGps()}");
+    Console.WriteLine();
+}
+
+// Part 2
+{
+    var (sokoban, directions) = Sokoban.ParseInput(input, true);
+    sokoban.PrintMap();
+    foreach (var direction in directions) sokoban.Move(direction);
+    Console.WriteLine($"Part 2 = {sokoban.GetSumGps()}");
+}
--- 2024/day15/Sokoban.cs
+++ 2024/day15/Sokoban.cs
@@ -10,72 +10,137 @@ public class Sokoban
         Left
     }
 
-    public enum Space
+    private enum Space
     {
         Void,
-        Box,
+        SimpleBox,
         Robot,
-        Wall
+        Wall,
+        LargeBoxLeft,
+        LargeBoxRight
     }
 
-    public char[][] Map;
-    public (int, int) RobotPosition;
+    private char[][] Map;
+    private (int, int) RobotPosition;
 
-    public Sokoban(string[] map)
+    private Sokoban(string[] map, bool part2 = false)
     {
-        Map = map.Select(x => x.ToCharArray()).ToArray();
+        var actualMap = map.Select(x => x.ToCharArray()).ToArray();
+        if (!part2)
+        {
+            Map = actualMap;
+        }
+        else
+        {
+            // Resize the map
+            Map = new char[actualMap.Length][];
+            for (var y = 0; y < Map.Length; y++)
+            {
+                Map[y] = new char[actualMap[0].Length * 2];
+                for (var x = 0; x < actualMap[y].Length; x++)
+                    switch (GetSpace(actualMap[y][x]))
+                    {
+                        case Space.Robot:
+                            Map[y][2 * x] = '@';
+                            Map[y][2 * x + 1] = '.';
+                            break;
+                        case Space.Void:
+                            Map[y][2 * x] = '.';
+                            Map[y][2 * x + 1] = '.';
+                            break;
+                        case Space.Wall:
+                            Map[y][2 * x] = '#';
+                            Map[y][2 * x + 1] = '#';
+                            break;
+                        case Space.SimpleBox:
+                            Map[y][2 * x] = '[';
+                            Map[y][2 * x + 1] = ']';
+                            break;
+                    }
+            }
+        }
+
+
         RobotPosition = FindRobotPosition();
     }
 
     // Move the robot in the given direction
-    public bool Move(Direction direction)
+    public void Move(Direction direction)
     {
-        var move = Move(RobotPosition, direction);
+        Move(RobotPosition, direction);
         RobotPosition = FindRobotPosition();
-        return move;
     }
 
-    public int GetSumGPS()
+    public int GetSumGps()
     {
         var gps = 0;
         for (var y = 0; y < Map.Length; y++)
         for (var x = 0; x < Map[y].Length; x++)
-            if (GetSpace((y, x)) == Space.Box)
+            if (GetSpace(Map[y][x]) == Space.SimpleBox || GetSpace(Map[y][x]) == Space.LargeBoxLeft)
                 gps += 100 * y + x;
-
         return gps;
+    }
+
+    // Check if a move is possible (recursively)
+    private bool CanMove((int, int) position, Direction direction)
+    {
+        var adjacentPosition = GetAdjacentPosition(position, direction);
+        if (adjacentPosition == null)
+            return false;
+
+        var space = GetSpace(Map[adjacentPosition.Value.Item1][adjacentPosition.Value.Item2]);
+        if (space == Space.Wall)
+            return false;
+        if (space == Space.Void)
+            return true;
+        if (space == Space.SimpleBox || ((space == Space.LargeBoxLeft || space == Space.LargeBoxRight) &&
+                                         (direction == Direction.Left || direction == Direction.Right)))
+            return CanMove(adjacentPosition.Value, direction);
+        if (space == Space.LargeBoxLeft || space == Space.LargeBoxRight)
+        {
+            var dd = space == Space.LargeBoxLeft ? Direction.Right : Direction.Left;
+            var otherAdjacentPosition = GetAdjacentPosition(
+                (adjacentPosition.Value.Item1, adjacentPosition.Value.Item2),
+                dd);
+            return CanMove(adjacentPosition.Value, direction) && otherAdjacentPosition.HasValue &&
+                   CanMove(otherAdjacentPosition.Value, direction);
+        }
+
+        throw new ArgumentException(
+            $"Illegal move (position {position.Item1}, {position.Item2} in direction {direction}");
     }
 
     // Perform a move for what's at the given position in the given direction
     // Return false if no move can be completed
-    private bool Move((int, int) position, Direction direction)
+    private void Move((int, int) position, Direction direction)
     {
-        var adjacentPosition = GetAdjacentPosition(position, direction);
+        if (!CanMove(position, direction))
+            return;
 
-        // Cannot move (out of bound)
-        if (adjacentPosition == null)
-            return false;
-
-        switch (GetSpace(adjacentPosition.Value))
+        var adjacentPosition = GetAdjacentPosition(position, direction)!.Value;
+        var space = GetSpace(Map[adjacentPosition.Item1][adjacentPosition.Item2]);
+        if (space == Space.Void)
         {
-            case Space.Wall:
-                // Cannot move into a wall
-                return false;
-            case Space.Void:
-                // Move into a void!
-                Map[adjacentPosition.Value.Item1][adjacentPosition.Value.Item2] = Map[position.Item1][position.Item2];
-                Map[position.Item1][position.Item2] = '.';
-                return true;
-            case Space.Box:
-                // Need to recursively check if we can move the box
-                var recursiveMove = Move(adjacentPosition.Value, direction);
-                if (!recursiveMove)
-                    return false;
-                Map[adjacentPosition.Value.Item1][adjacentPosition.Value.Item2] = Map[position.Item1][position.Item2];
-                Map[position.Item1][position.Item2] = '.';
-                return true;
-            default:
-                throw new ArgumentException("Illegal move");
+            Map[adjacentPosition.Item1][adjacentPosition.Item2] = Map[position.Item1][position.Item2];
+            Map[position.Item1][position.Item2] = '.';
+        }
+        else if (space == Space.SimpleBox || ((space == Space.LargeBoxLeft || space == Space.LargeBoxRight) &&
+                                              (direction == Direction.Left || direction == Direction.Right)))
+        {
+            Move(adjacentPosition, direction);
+            Map[adjacentPosition.Item1][adjacentPosition.Item2] = Map[position.Item1][position.Item2];
+            Map[position.Item1][position.Item2] = '.';
+        }
+        else if (space == Space.LargeBoxLeft || space == Space.LargeBoxRight)
+        {
+            var dd = space == Space.LargeBoxLeft ? Direction.Right : Direction.Left;
+            var otherAdjacentPosition = GetAdjacentPosition(
+                (adjacentPosition.Item1, adjacentPosition.Item2),
+                dd)!.Value;
+            Move(otherAdjacentPosition, direction);
+            Move(adjacentPosition, direction);
+            Map[adjacentPosition.Item1][adjacentPosition.Item2] = Map[position.Item1][position.Item2];
+            Map[position.Item1][position.Item2] = '.';
         }
     }
 
@@ -95,16 +160,15 @@ public class Sokoban
     {
         for (var y = 0; y < Map.Length; y++)
         for (var x = 0; x < Map[y].Length; x++)
-            if (GetSpace((y, x)) == Space.Robot)
+            if (GetSpace(Map[y][x]) == Space.Robot)
                 return (y, x);
 
         throw new ArgumentException("Unable to find robot initial position");
     }
 
-    private Space GetSpace((int, int) location)
+    private Space GetSpace(char space)
     {
-        var (y, x) = location;
-        switch (Map[y][x])
+        switch (space)
         {
             case '.':
                 return Space.Void;
@@ -113,12 +177,17 @@ public class Sokoban
             case '#':
                 return Space.Wall;
             case 'O':
-                return Space.Box;
+                return Space.SimpleBox;
+            case '[':
+                return Space.LargeBoxLeft;
+            case ']':
+                return Space.LargeBoxRight;
             default:
                 throw new ArgumentException("Unknown space");
         }
     }
 
+    // Return the adjacent position in the given direction
     private (int, int)? GetAdjacentPosition((int, int) position, Direction direction)
     {
         var (y, x) = position;
@@ -154,7 +223,7 @@ public class Sokoban
         }
     }
 
-    public static (Sokoban, List<Direction>) ParseInput(string input)
+    public static (Sokoban, List<Direction>) ParseInput(string input, bool part2 = false)
     {
         var map = new List<string>();
         List<Direction> directions = new();
@@ -165,7 +234,7 @@ public class Sokoban
             if (sokoban == null)
             {
                 if (string.IsNullOrEmpty(line))
-                    sokoban = new Sokoban(map.ToArray());
+                    sokoban = new Sokoban(map.ToArray(), part2);
                 else
                     map.Add(line);
             }
